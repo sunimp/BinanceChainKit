45b05d5061d37949c6b516b7fbc5e10c1b791994
杨光 <yangguang@webull.com>
2024-08-23 19:17:26 +0800

    fix: upgrade dependencies

--- iOS Example/Sources/Controllers/WordsController.swift
+++ iOS Example/Sources/Controllers/WordsController.swift
@@ -101,7 +101,7 @@ class WordsController: UIViewController {
 
             try Manager.shared.login(words: words)
 
-            if let window = UIApplication.shared.activeWindow {
+            if let window = UIWindow.keyWindow {
                 UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: {
                     window.rootViewController = MainController()
                 })
